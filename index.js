72fa8c003507db8c533c537c9859c98b56ef310b
Sadek Hossain Nishat <nishat@akijgroup.co>
2024-02-19 14:36:24 +0600

    third commit 19th february 2024

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import GradientText from './components/GradientText';
+import CalculatorApp from './calculatorappcomponents/CalculatorApp';
 
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => GradientText);
+// AppRegistry.registerComponent(appName, () => GradientText);
+AppRegistry.registerComponent(appName, () => CalculatorApp);
